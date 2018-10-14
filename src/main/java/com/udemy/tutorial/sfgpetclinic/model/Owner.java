415beede87e17f0269a7c1acd1fc93e9f43a1bfb
DevendraBahirat <devendra.bahirat@gmail.com>
2018-10-14 22:40:03 +0530

    Adding Pojo

---
+++ src/main/java/com/udemy/tutorial/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.udemy.tutorial.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/udemy/tutorial/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.udemy.tutorial.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
