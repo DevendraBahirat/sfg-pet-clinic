8ad28afa07e5ff723046570040aaa6f6c11360ac
DevendraBahirat <devendra.bahirat@gmail.com>
2018-10-15 23:28:14 +0530

    Refactoring to common CRUD interface

--- pet-clinic-data/src/main/java/com/udemy/tutorial/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/udemy/tutorial/sfgpetclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package com.udemy.tutorial.sfgpetclinic.services;
 
 import com.udemy.tutorial.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/udemy/tutorial/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/udemy/tutorial/sfgpetclinic/services/PetService.java
@@ -2,13 +2,5 @@ package com.udemy.tutorial.sfgpetclinic.services;
 
 import com.udemy.tutorial.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
